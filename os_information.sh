2e3eafb3e2afa3984c6e2c136e0f47a641a4e945
Thummims <86824763+Thummims@users.noreply.github.com>
2021-08-11 10:42:00 +0800

    Update os_information.sh

--- os_information.sh
+++ os_information.sh
@@ -6,3 +6,9 @@ dmidecode -s bios-version
 
 #DIMM
 dmidecode |grep "Speed"
+
+# 操作系统下disabled HT.
+echo off > /sys/devices/system/cpu/smt/control
+
+# disabled turbo
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
